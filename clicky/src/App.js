a07d1f425b9903033242698d4c1cc0f411fe2ca6
Lindsey Bowen <lindsbow102@yahoo.com>
2018-05-16 12:09:27 -0700

    changed css

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        <Header score={this.state.score} highscore={this.state.highscore}>Candy Clicker Game</Header>
         {this.state.cards.map(card => (
           <Card
             clickCount={this.clickCount}
